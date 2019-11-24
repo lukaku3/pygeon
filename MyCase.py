d810ee9393d26cddfec746450fbee9146650be5b
John Doe <johndoe@example.com>
2019-11-24 22:41:54 +0900

    add sub

--- MyCase.py
+++ MyCase.py
@@ -11,20 +11,24 @@ from selenium.webdriver.chrome.options import Options
 from sub import subClass
 
 class MyTestCase(unittest.TestCase):
+    selenium_server = 'http://localhost:4444/wd/hub'
+    # selenium_server = 'http://192.168.33.1:4444/wd/hub'
 
     def setUp(self):
-        # ----- launch visible
-        #        self.driver = webdriver.Remote(
-        #           command_executor= self.selenium_server,
-        #            desired_capabilities=DesiredCapabilities.CHROME)
-        # ----- launch background start
+
         opts = Options()
         # opts.binary_location = self.browser_path
-        opts.add_argument('--headless')
+        # opts.add_argument('--headless')
         opts.add_argument('--disable-gpu')
         opts.add_argument('--no-sandbox')
-        self.driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=opts)
-        # self.driver = webdriver.Chrome(executable_path="/c/Users/kazuh/bin/chromedriver", chrome_options=opts)
+        # self.driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=opts)
+        # ----- launch remote start
+        self.driver = webdriver.Remote(
+        #     command_executor= self.selenium_server,
+            desired_capabilities=DesiredCapabilities.CHROME,
+            options=opts
+        )
+        # ----- launch remote end
 
     def test_something(self):
         subClass.display(self, self.driver)
